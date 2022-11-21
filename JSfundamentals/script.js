ce1c7ee58c0cc41a9aa4885c4424f69e37cd267b
uniq47 <unique.sapkota01@student.csulb.edu>
2022-11-20 16:38:03 -0800

    datatypes js

--- JSfundamentals/script.js
+++ JSfundamentals/script.js
@@ -1,2 +1,40 @@
-alert("hello");
-console.log("hello from js");
+//const 
+//var 
+//let   these are the three ways to declare a variable
+
+const bootcamp = "python";
+console.log(typeof bootcamp);
+
+const myCountry= "Nepal";
+console.log(myCountry);
+
+const age=45;
+console.log(typeof age);
+
+//primitive: Number, String, Boolean, Null, Undefined, Symbol
+//non-primitive: Object, Array, Function-> all are objects in JS 
+
+document.write("hello") //document.write is used to write in html file in js
+console.log(document)
+
+//const evenNumbers=[2,"hello",4,6,8,10,[1,2]];
+const evenNumbers= new Array(2,"hello",4,6,8,10,[1,2]);
+console.log(evenNumbers[1])
+
+//objects in js so 
+const bootcourse={
+    course: 'React',
+    duration: "3 months",
+};
+
+const arrayBootcamp=["React","3 months "]
+console.log(bootcourse[1]);
+
+//functions in js
+
+function addTwoNumbers(first, second){
+    return first+second;
+}
+
+const result = addTwoNumbers(1,2);
+console.log(result);
