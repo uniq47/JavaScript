1d321c49653120d4249f26fe9aec13794e0364ec
uniq47 <unique.sapkota01@student.csulb.edu>
2022-11-22 08:52:04 -0800

    spread and rest, destructuring , fucntions, loops if missed

--- JSfundamentals/script.js
+++ JSfundamentals/script.js
@@ -1,3 +1,109 @@
+//for loop
+//for Off
+//for in 
+//while loop
+
+
+//for Each
+//Map
+//filter
+//reduce
+
+//const arr=[7,8,9,10];
+//imperative
+// for (let i =0;i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+//declarative code/ get the values of the array
+// for(number of arr){
+//     console.log(number)
+// }
+
+// for(i in arr){
+//     console.log(arr[i]);
+// }
+//if we dont know when to end then we use while loop
+// let index=0;
+// while(index<8){
+//     console.log("runnding ")
+//     index++;
+// }
+
+// imp in react
+
+//forEach, map, filter, reduce
+
+// function display(abcd){
+//     // console.log(a);
+//     abcd(10);// calls display(function displayNumber(number){console.log(number)});
+// }
+//call back function
+//displaying function inside the function as a paramneter is called callback function
+//we can pass number array everyhting as a parameter
+// display(function (number){
+//     console.log(number)
+// });
+
+// function displayName(number){
+//     console.log(number);
+// }
+
+
+//const arr=[7,8,9,10,20 ];
+//console.log(arr);
+
+// function arrayFunction(){
+//     console.log("Running");
+
+//for each will call the finction for each element in the array
+// for each will accept only function
+// arr.forEach(function arrayFunction(){
+//          console.log("Running")});
+
+//arr.forEach((item,index )=>console.log(index));
+
+//arr.forEach((item)=>console.log(item));
+//array
+
+
+
+
+
+const obj={
+    name: 'unique',
+    age:23,
+    favNumber:[1,2,3,4],
+    calcAge: function(){
+        console.log('running calcAge function');// functoin insde a object is called method
+        return 'your age';
+    },
+    freeHours:{
+        hourOne:'six-seven',
+        hourTwo:2100,
+        allhours:[1800,1900,2100]
+    }
+};
+
+
+
+// const availableHours = obj.freeHours.allhours[0];
+// console.log(availableHours);
+
+
+//object distructuring 
+
+
+const {age:myAge,
+    favNumber,
+    freeHours:{
+        hourOne,
+        allhours:[firstHour]},
+}=obj //myAge is the alias of age
+console.log(myAge);
+console.log(favNumber);
+console.log(firstHour);
+
+
 
 
 
