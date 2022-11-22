192a36d07b39bc18fe61e7b66bac3676c41b0938
uniq47 <unique.sapkota01@student.csulb.edu>
2022-11-22 13:39:59 -0800

    foreach, map, reduce, filter

--- JSfundamentals/script.js
+++ JSfundamentals/script.js
@@ -20,12 +20,50 @@
 
 // if we want to return we use map method 
 
-const arr=[1,2,3,4]
-const newArr=[];
-console.log(newArr);
+const arr=[10,20,30,40]
 
-const resut = arr.forEach((el) => {
-    newArr.push(el)
+
+// for each will return undefined as it is not returning anything. we do internal thing for array,ex push 
+const resultForEach= arr.forEach( (el,i,array)=> {
+    //     console.log(i)
+    //     console.log(el)
+    //     console.log(array)
+      //  return el*2;
+     })
+    
+    console.log(resultForEach);
+
+const resut = arr.map((el,i,array) => { //map method is excpein tg each and every iteration or getting elements .
+    //map gets elemnts automatically, it gets elemnts 
+//    console.log(el);
+//     console.log(i)
+    
+//     console.log(array)
+    return el*2; // always returns array of same lenght
 });
+console.log(resut);
 
-console.log(newArr);
+//console.log(newArr);
+
+
+// in filter we use condition to filter out the elements 
+const filterOutput= arr.filter( (el,i,array)=> {
+//     console.log(i)
+//     console.log(el)
+//     console.log(array)
+    return el>20;
+ })
+
+console.log(filterOutput); // this function is not returning any thing 
+
+
+
+
+const reduceOutput= arr.reduce( (acc,currentValue)=> {
+    console.log(acc)
+    //console.log(currentValue);
+    return acc+ currentValue;
+}, 0 )// we add two arugment inside reduce method  retuns only one value
+    
+    
+    console.log(reduceOutput);
