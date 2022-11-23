f688556d780c8631c75780d861d7463eb9557403
uniq47 <unique.sapkota01@student.csulb.edu>
2022-11-22 19:30:23 -0800

    strict mode

--- JSfundamentals/script.js
+++ JSfundamentals/script.js
@@ -1,35 +1,16 @@
+
+//strict model
 "use strict";
 
 // to create a schema or blueprint for our data
 //consctructor function
 
 
- // this keyword-> // this keyword points to the object that is calling the method
-
- console.log(this)
- // points to the gobal object comming from the web api, pointing to the golbal window object
- // dont always point to the global object
-
- const obj={
-    name:"unique",
-    age:31,
-    calcAge: function(){
-        console.log(this);
- },
-};
-
-obj.calcAge();
 
 
-function add(){
-    console.log(this);
-    if(true){
-        console.log(this);
-    }
+//const private ="Hospital";
+
+for ( i=0; i<10; i++){
+    console.log(i);
 }
-add();
 
-
-if(true){
-    console.log(this);
-}
