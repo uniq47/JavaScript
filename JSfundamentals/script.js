a4e4b3d731c8eb04dc0a6b44d7efa9466c5821ce
uniq47 <unique.sapkota01@student.csulb.edu>
2022-11-23 09:24:34 -0800

    object, constructor and addig properties

--- JSfundamentals/script.js
+++ JSfundamentals/script.js
@@ -1,52 +1,27 @@
+'use strict';
 
-// //strict model
-// "use strict";
-
-
- const obj={
-    name:"unique",
-    age:31,
-    calcAge: function(){
-        console.log(this);
- },
- favNumbers:{
-    firstNumber:1,
-    secondNumber:2,
- },
- totalNumber:100
+//constructor function
+// make schema from where we make multiple objects
+// when we make a constructor function we create it with a capital leter
+const Person = function (firstName){ // parameter is firstName
+    this.first = firstName;// or this.a=firstName; or this.firstName=firstName;//fiorsti s the properties of the object
+//if we write this keyword inside the object , it will 
 };
-// optional patrameter
-console.log(obj.favNumbers?.firstNumber);
+
+//how to create a object , we created a object from the constructor function Person
+const uniqObj =new Person ('unique') //argument is uniqe
+console.log(uniqObj);
+
+const newObj =new Person ('aayam') //argument is new
+console.log(uniqObj);
 
 
-// //?? operator
-// 0, false, null, undefined, NaN, '' are falsy values
-//removing one of the falsy values 
-
-totalNumber= false
-//shoirt circuit operator
-const totalProducts = totalNumber || 20;
-
-// console.log(totalProducts);
-
-// if(totalNumber){
-//     console.log("you have total products ");}
-// else{
-//     console.log("you have no products");
-// }
-
-//shoirt surcuting operator
-// || operator stopes at truthy value 
-//&& operator stopes at falsy value  
-// if cant find the truthy value it will return the last value the one on the end 
-// console.log(0 && 1 && 2);
 
 
-//obj.totalNumber=0
-const number = obj.totalNumber ?? 2000;
-console.log(number);
 
+//static constructor function passing uniq in this  and not the parameter
+// const Person1 = function (){
+//     this.firstName= "uniq";
+// };
 
-// let height=0;
-// alert(height || 100); //100
-// alert(height ?? 100); //0 so the main aim of the ?? is to make the _0_ falsy value to be truthy value
+// console.log(Person1);
