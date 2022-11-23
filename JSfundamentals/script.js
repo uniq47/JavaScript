7a8c4f139ab54d790a481860bafd63b393f8d5df
uniq47 <unique.sapkota01@student.csulb.edu>
2022-11-22 17:32:25 -0800

    this keyword

--- JSfundamentals/script.js
+++ JSfundamentals/script.js
@@ -1,69 +1,35 @@
-// const arr=[1,2,3,4]
+"use strict";
+
+// to create a schema or blueprint for our data
+//consctructor function
 
 
-// //fucntion inside of the function
+ // this keyword-> // this keyword points to the object that is calling the method
+
+ console.log(this)
+ // points to the gobal object comming from the web api, pointing to the golbal window object
+ // dont always point to the global object
+
+ const obj={
+    name:"unique",
+    age:31,
+    calcAge: function(){
+        console.log(this);
+ },
+};
+
+obj.calcAge();
 
 
+function add(){
+    console.log(this);
+    if(true){
+        console.log(this);
+    }
+}
+add();
 
 
-// const result = arr.forEach((el)=> { // we  created a function inside of the function wl ia  FUCNTOIN WE CREATED WHIHC ARROW FUNCTION
-// console.log(el);
-// return "hello";
-// });
-// console.log(result);
-
-// const result1 = arr.map((el)=> 20) //dont have to write return as it will automaticall return 20, we use curly brace when there is multiple line s
-//     console.log(result1);
-    
-
-// const  add = () => 10 // with array function we can remove the array {}
-
-// if we want to return we use map method 
-
-const arr=[10,20,30,40]
-
-
-// for each will return undefined as it is not returning anything. we do internal thing for array,ex push 
-const resultForEach= arr.forEach( (el,i,array)=> {
-    //     console.log(i)
-    //     console.log(el)
-    //     console.log(array)
-      //  return el*2;
-     })
-    
-    console.log(resultForEach);
-
-const resut = arr.map((el,i,array) => { //map method is excpein tg each and every iteration or getting elements .
-    //map gets elemnts automatically, it gets elemnts 
-//    console.log(el);
-//     console.log(i)
-    
-//     console.log(array)
-    return el*2; // always returns array of same lenght
-});
-console.log(resut);
-
-//console.log(newArr);
-
-
-// in filter we use condition to filter out the elements 
-const filterOutput= arr.filter( (el,i,array)=> {
-//     console.log(i)
-//     console.log(el)
-//     console.log(array)
-    return el>20;
- })
-
-console.log(filterOutput); // this function is not returning any thing 
-
-
-
-
-const reduceOutput= arr.reduce( (acc,currentValue)=> {
-    console.log(acc)
-    //console.log(currentValue);
-    return acc+ currentValue;
-}, 0 )// we add two arugment inside reduce method  retuns only one value
-    
-    
-    console.log(reduceOutput);
+if(true){
+    console.log(this);
+}
