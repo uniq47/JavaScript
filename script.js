c2abdf66fa8fb2816389f25ce681b010b271159f
uniq47 <unique.sapkota01@student.csulb.edu>
2022-12-02 17:25:51 -0800

    async await

--- script.js
+++ script.js
@@ -1,70 +1,32 @@
 'use strict';
 
-const url="https://api.github.com/users/uniq47";
+const url="https://api.github.com/users/";
+const usernameInput = "uniq47";
 
-console.log(window);
-
-// const response = fetch(url)
-
-// console.log(response);
-
-// response.then(function () {
-//     console.log("Promise is resolved");
-// });
-
-// response.catch(function (){
-//     console.log("Promise is rejected");
-// })
-
-// const response = fetch(url)//fetched the result form url
-// .then(function(){
-//     console.log("promise if fullfilled")
-// })
-// .catch(function(){
-//     console.log("promise is rejected")
-// });
-
-// //fetched the result form url
-// const response = fetch(url)
-// .then((res) => res.json())//one is for fetch(url) 
-// .then((data)=>console.log(data))//and other is for response.json()
-// .catch((error)=>console.log(error));
-
-// const fetchProfile = async () => {
-//     const res = await fetch(url);
-//     const data = await res.json();
-//         console.log(data);
-
-// }
-// using immediately invoked function expression 
-(async () => {
-    
-    try{
-        const res = await fetch(url);
-        const data = await res.json();
-    }catch(error){
-        console.log("error occured")
-    }
-
-        console.log(data)})();
 
 const fetchProfile= async () => {
-    
-            try{
-                const res = await fetch(url);
-                const data = await res.json();
-            }catch(error){
-                console.log("error occured")
-            }
+    try{//if want to install js insdie your string tben use ``
+        const res = await fetch(`${url}${usernameInput}`);
+        const data = await res.json();
+       }catch(error){
+        console.log("error occured")
         }
-
-        fetchProfile();
-
-// cant do catch in async function
-
-//we can use try catch block in async function
+};
+fetchProfile();
 
 
-//.catch(() => console.log("promise is rejected"));
+const fetchRepos = async () => {
+    try{//get the repos of the user 
+        const res = await fetch(`${url}${usernameInput}/repos`);
+        //get the data in json format from the response object and store it in data variable
+        const data = await res.json();
+        console.log(data);  
+    }catch(error){
+        console.log(error)
+    }
+}
+
+fetchRepos();
+
 
 
